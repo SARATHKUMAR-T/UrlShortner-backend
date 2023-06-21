8cb5f6e79a7f973b4828ca58e4e7fd2ae476c435
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-06-21 13:02:07 +0530

    index change

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-import express from "express"
-import "dotenv/config"
-import dbConnnection from "./db.js"
-import { Url } from "./Models/shortUrl.js"
+import express from "express";
+import "dotenv/config";
+import dbConnnection from "./db.js";
+import { Url } from "./Models/shortUrl.js";
+import cors from "cors";
 
+const app = express();
 
-const app=express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // db connection
 
-dbConnnection()
+dbConnnection();
 
 // api
-app.post("/short",async(req,res)=>{
-    try {
+app.post("/short", async (req, res) => {
+  try {
+    const full = await Url.create({ full: req.body.fullUrl });
 
-      const full= await Url.findOne({full:req.body.fullUrl})
+    res.status(200).json({ message: "url fetched successfully", full });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
 
-      res.status(200).json({message:"url fetched successfully" ,full})
+app.get("/:shortUrl", async (req, res) => {
+  try {
+    const shortId = await req.params.shortUrl;
 
-      if(!full){
-        const newUrl=await Url.create({full:req.body.fullUrl})
-        res.status(200).json({message:'new url created successfully',newUrl})
-        
-      }
-     
+    console.log(shortId);
+    const final = await Url.findOne({ short: shortId });
+    console.log(final.full);
+    console.log('successfully redirected');
+    res.redirect(final.full);
 
-      
-        
-    } catch (error) {
-        res.status(500).json({message:'Internal server error',error})
-    }
-})
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
 
-app.listen(process.env.PORT,()=>console.log('server started'))
+app.listen(process.env.PORT, () => console.log("server started"));
